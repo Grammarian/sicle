6a82dd3c5afde57c2878ec65d0db8f197b9ff5bc
Phillip Piper <phillip.piper@gmail.com>
2018-03-13 21:14:26 +1100

    Remove early experiments

--- rollcall-mark-i/src/Components/PageFooter.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class PageFooter extends Component {
-  render() {
-    return <p>page footer</p>;
-  }
-}
-
-export default PageFooter;
--- rollcall/src/Components/PageHeader.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from "react";
-
-class PageHeader extends Component {
-  render() {
-    return <p>page header</p>;
-  }
-}
-
-export default PageHeader;
